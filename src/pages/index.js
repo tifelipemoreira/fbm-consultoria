eca013fb16204565ab62a22a8c7f34c53e736b6a
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-08-05 13:23:31 -0300

    melhoria imagem

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import Heads from "../components/Heads/Heads";
 import NavBar from "../components/NavBar/Navbar";
 import Footer from "../components/Footer/Footer";
+import supportImg from "../../img/imgsite01.png";
+import Image from "next/image";
+
 // exemplos https://github.com/ndpniraj/react-ecom/tree/master/src/components
 //https://github.com/Sridhar-C-25/reacttailwind_footer
+
+//bom exemplo
+// https://github.com/fireclint/cloud-app-react/
 export default function Home() {
   return (
     <div>
@@ -14,6 +20,9 @@ export default function Home() {
         href="/favicon.ico"
       ></Heads>
       <NavBar></NavBar>
+      <div className='w-full h-[100px]  absolute'>
+        <Image className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" width={1536} height={774}/>
+      </div>
       <div className="flex justify-center items-center w-full h-96 border-4 border-red-900">
         <div>
           <h1 className="text-center">
