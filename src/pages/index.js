8586e7e2540d61ed1f2a17cc456e0a51bf594f24
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-08-05 13:25:28 -0300

    melhorias

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,9 +21,9 @@ export default function Home() {
       ></Heads>
       <NavBar></NavBar>
       <div className='w-full h-[100px]  absolute'>
-        <Image className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" width={1536} height={774}/>
+        <Image className='w-full h-full object-cover mix-blend-overlay' src={supportImg} alt="/" width={1536} height={775}/>
       </div>
-      <div className="flex justify-center items-center w-full h-96 border-4 border-red-900">
+      <div className="flex justify-center items-center w-full h-[770px] border-4 border-red-900">
         <div>
           <h1 className="text-center">
             Serviços de Criação de Sites<br></br>
@@ -31,7 +31,7 @@ export default function Home() {
           </h1>
         </div>
       </div>
-      <div className="flex justify-center items-center w-full h-96 border-4 border-red-900">
+      <div className="flex justify-center items-center w-full h-[770px] border-4 border-red-900">
         <div>
           <h1 className="text-center">
             Consultoria ERP<br></br>
